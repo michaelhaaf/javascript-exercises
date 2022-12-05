93bfd2e1d6ef73597a67b0ebc89a12c269e770ca
michaelhaaf <michael.haaf@gmail.com>
2022-12-05 16:03:43 -0500

    complete javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  result = "";
+  chars = string.split("");
 
+  for (let i=chars.length-1; i>=0; i--) {
+    result += chars[i]
+  }
+
+  return result;
 };
 
 // Do not edit below this line
