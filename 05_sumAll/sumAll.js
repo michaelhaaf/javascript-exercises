ee1232ad180c46ce7497a08264947df41ab51c8a
michaelhaaf <michael.haaf@gmail.com>
2022-12-05 18:27:51 -0500

    complete javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+function sumOne(n) {
+  return ((n + 1) * n) / 2;
+}
 
+function isValid(n) {
+  if (typeof n != "number") return false;
+  return !isNaN(n) && n >= 0;
+}
+
+const sumAll = function (num1, num2) {
+  if (!isValid(num1) || !isValid(num2)) {
+    return "ERROR";
+  }
+  let larger = Math.max(num1, num2);
+  let smaller = Math.min(num1, num2);
+  return sumOne(larger) - sumOne(smaller) + smaller;
 };
 
 // Do not edit below this line
