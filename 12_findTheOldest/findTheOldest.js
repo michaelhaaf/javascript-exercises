9e9e0a88603066caf939050e04b260db5ad750e9
michaelhaaf <michael.haaf@gmail.com>
2022-12-30 14:03:29 -0500

    complete javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((previous, current) => {
+    if (getAge(current) < getAge(previous)) {
+      return previous;
+    } else {
+      return current;
+    }
+  });
 };
+
+function getAge(person) {
+  if (typeof person.yearOfDeath === "undefined") {
+    person.yearOfDeath = new Date().getFullYear();
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
