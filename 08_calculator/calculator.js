6f8476f45183361b7cdf2abe515467700c1164b1
michaelhaaf <michael.haaf@gmail.com>
2022-12-30 12:59:28 -0500

    complete javascript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  // if (numbers.length === 0) { return 0; }
+  return numbers.reduce((previous, current) => {
+    return add(previous, current);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((previous, current) => {
+    return previous * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  result = 1;
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
