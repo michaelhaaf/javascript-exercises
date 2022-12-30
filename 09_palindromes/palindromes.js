e7458213aba5579047a3f028e1ff228a855ce6ed
michaelhaaf <michael.haaf@gmail.com>
2022-12-30 13:27:55 -0500

    complete javascript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  const nonAlphanumericRE = /[^A-Za-z0-9]/g;
+  let palindrome = input.toLowerCase().replace(nonAlphanumericRE, '');
 
+  let n = palindrome.length; 
+  let max_index = Math.floor(n/2);
+
+  for (let i=0; i<max_index; i++) {
+    if (palindrome[i] != palindrome[n-1-i]) {return false;}
+  }
+  return true;
 };
 
 // Do not edit below this line
